932190d2ae06601721cbd2549cbcae4d2528299b
Uplink036 <oliversjodin02@gmail.com>
2024-10-01 09:18:48 +0000

    Add MVC .net web template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StadewCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
