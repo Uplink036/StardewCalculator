bb323fc7bdf61ef485662712b7841e36ae1f9479
Uplink036 <oliversjodin02@gmail.com>
2024-10-10 13:11:14 +0000

    Add new class StringCalculator

---
+++ Models/StringCalculator.cs
@@ -0,0 +1,14 @@
+namespace StadewCalculator.Models;
+
+public class StringCalculator
+{
+    public float stringToFloat(string input){
+        // Check if empty
+        if (string.Compare(input, "") == 0)
+        {
+            return 0.0F;
+        }
+        // Return value
+        return float.Parse(input);
+    }
+}
