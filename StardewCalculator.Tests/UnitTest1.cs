513493668331cf47efe6e407326a151d917d3709
Sofia Blom (Ned) <sofia@familjenblom.se>
2024-10-10 19:59:58 +0200

    Add tests for String Calculator

--- StardewCalculator.Tests/UnitTest1.cs
+++ StardewCalculator.Tests/UnitTest1.cs
@@ -1,5 +1,5 @@
 namespace StardewCalculator.Tests;
-
+using StadewCalculator.Models;
 public class Tests
 {
     [SetUp]
@@ -10,6 +10,21 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        StringCalculator testObject = new StringCalculator();
+        Assert.IsNotNull(testObject);
+    }
+    [Test]
+    public void TeststringToFloatExists()
+    {
+        StringCalculator testObject = new StringCalculator();
+        Assert.IsNotNull(testObject.stringToFloat(""));
+    }
+    [Test]
+    public void TeststringToFloatReturns()
+    {
+        StringCalculator testObject = new StringCalculator();
+        Assert.AreEqual(testObject.stringToFloat("0"), 0.0F);
+        Assert.AreEqual(testObject.stringToFloat("-1"), -1.0F);
+        Assert.AreEqual(testObject.stringToFloat("1"), 1.0F);
     }
 }
