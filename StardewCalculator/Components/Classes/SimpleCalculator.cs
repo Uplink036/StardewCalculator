7a6b4345c3b495e840f190b7f5ca5a524fc3bcca
Uplink036 <Oliversjodin02@gmail.com>
2024-10-17 21:40:35 +0200

    Add SimpleCalculator

---
+++ StardewCalculator/Components/Classes/SimpleCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace StardewCalculator.Components.Classes
+{
+    public class SimpleCalculator
+    {
+        private float lhs, rhs;
+        private char op;
+        public SimpleCalculator() {
+            lhs = 0;
+            rhs = 0;
+            op = '+';
+        }
+
+        public float calculate()
+        {
+            switch (op)
+            {
+                case '+':
+                    lhs = lhs + rhs;
+                    break;
+                case '-':
+                    lhs = lhs - rhs;
+                    break;
+                case '*':
+                    lhs = lhs * rhs;
+                    break;
+                case '/':
+                    lhs = lhs / rhs;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+            return lhs;
+        }
+
+        public void setOp(char op)
+        { 
+            this.op = op; 
+        }
+
+        public void add(float input)
+        {
+            rhs = rhs * 10 + input; // Add the input to the number
+        }
+    }
+}
