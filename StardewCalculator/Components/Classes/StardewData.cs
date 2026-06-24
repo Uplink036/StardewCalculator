db7ed62f01ec378771625ff0d5b7e07123966d80
Sofia Blom - Ender <sofia@familjenblom.se>
2026-06-24 14:40:52 +0200

    Loading in data

--- StardewCalculator/Components/Classes/StardewData.cs
+++ StardewCalculator/Components/Classes/StardewData.cs
@@ -1,6 +1,9 @@
 namespace StardewCalculator.Components.Classes;
 
+using System.Globalization;
 using CsvHelper;
+using System.Diagnostics.CodeAnalysis;
+
 
 public enum Shop
 {
@@ -16,22 +19,43 @@ public class StardewData
 
     bool vanillaShop { get; set; }
 
-    public IEnumerable<ShopStock> data;
+    public List<ShopStock> data;
 
     public StardewData(bool vanillaShop, Shop shop = Shop.PIERRE)
     {
         this.vanillaShop = vanillaShop;
         CurrentShop = shop;
-
-        // var test = new ShopStock();
-        // Load data?
+        LoadData();
     }
 
 
+    [MemberNotNull(nameof(data))]
     private void LoadData()
     {
-        // string filepath = "";
-        Console.WriteLine(Directory.GetCurrentDirectory());
+        string filepath = $@"data/{(vanillaShop ? "vanilla" : "expanded")}/";
+        switch (CurrentShop)
+        {
+            case Shop.PIERRE:
+                filepath += "pierres_general_store.csv";
+                break;
+            case Shop.JOJAMART:
+                filepath += "jojamart.csv";
+                break;
+            case Shop.OASIS:
+                filepath += "oasis.csv";
+                break;
+            default:
+                throw new Exception("Unknown shop. Unable to load data");
+        }
+
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filepath);
+        using (var reader = new StreamReader(path))
+        {
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                data = csv.GetRecords<ShopStock>().ToList<ShopStock>();
+            }
+        }
     }
     
 }
