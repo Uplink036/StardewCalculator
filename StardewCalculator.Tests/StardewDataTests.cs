ff17942dbdb8d457db1e11ada9055549bc040bee
Sofia Blom - Ender <sofia@familjenblom.se>
2026-06-24 14:34:47 +0200

    Test all the different types of shops

--- StardewCalculator.Tests/StardewDataTests.cs
+++ StardewCalculator.Tests/StardewDataTests.cs
@@ -1,12 +1,25 @@
 namespace StardewCalculator.Tests;
 using StardewCalculator.Components.Classes;
+
 public class StardewDataTests
 {
     [Test]
-    public void LoadData()
+    public void LoadAllShops()
     {
-        // StardewData dataHolder = new StardewData(true, Shop.PIERRE);
-        // Assert.That(true, false);
-        System.Console.WriteLine("Hello from a test");
+        var pierresVanilla = new StardewData(true, Shop.PIERRE);
+        var jojaVanilla = new StardewData(true, Shop.JOJAMART);
+        var oasisVanilla = new StardewData(true, Shop.OASIS);
+
+        var pierresExpanded = new StardewData(false, Shop.PIERRE);
+        var jojaExpanded = new StardewData(false, Shop.JOJAMART);
+        var oasisExpanded = new StardewData(false, Shop.OASIS);
+
+        Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6));
+        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1));
+        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1));
+
+        Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1));
+        Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1));
+        Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1));
     }
 }
