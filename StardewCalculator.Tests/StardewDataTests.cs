4a5417bd066c5fb0759bd02fbed7bf20a2f30aad
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-09-16 11:38:45 +0200

    Split LoadAllShops into indevidual tests

--- StardewCalculator.Tests/StardewDataTests.cs
+++ StardewCalculator.Tests/StardewDataTests.cs
@@ -4,22 +4,44 @@ using StardewCalculator.Components.Classes;
 public class StardewDataTests
 {
     [Test]
-    public void LoadAllShops()
+    public void LoadVanillaPierres()
     {
         var pierresVanilla = new StardewData(true, Shop.PIERRE);
-        var jojaVanilla = new StardewData(true, Shop.JOJAMART);
-        var oasisVanilla = new StardewData(true, Shop.OASIS);
-
-        var pierresExpanded = new StardewData(false, Shop.PIERRE);
-        var jojaExpanded = new StardewData(false, Shop.JOJAMART);
-        var oasisExpanded = new StardewData(false, Shop.OASIS);
-
         Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6));
-        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1));
-        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1));
+    }
 
+    [Test]
+    public void LoadVanillaJoja()
+    {
+        var jojaVanilla = new StardewData(true, Shop.JOJAMART);
+        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void LoadVanillaOasis()
+    {
+        var oasisVanilla = new StardewData(true, Shop.OASIS);
+        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void LoadExpandedPierres()
+    {
+        var pierresExpanded = new StardewData(false, Shop.PIERRE);
         Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void LoadExpandedJoja()
+    {
+        var jojaExpanded = new StardewData(false, Shop.JOJAMART);
         Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void LoadExpandedOasis()
+    {
+        var oasisExpanded = new StardewData(false, Shop.OASIS);
         Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1));
     }
 
