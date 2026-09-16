16f6a825f10c835b489c91319b5ec99da648ad78
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-09-16 11:56:30 +0200

    Add message to Load shop tests

--- StardewCalculator.Tests/StardewDataTests.cs
+++ StardewCalculator.Tests/StardewDataTests.cs
@@ -7,42 +7,42 @@ public class StardewDataTests
     public void LoadVanillaPierres()
     {
         var pierresVanilla = new StardewData(true, Shop.PIERRE);
-        Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6));
+        Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6), message=string.Format("Found crops: {0}", pierresVanilla.data));
     }
 
     [Test]
     public void LoadVanillaJoja()
     {
         var jojaVanilla = new StardewData(true, Shop.JOJAMART);
-        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1));
+        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1), message=string.Format("Found crops: {0}", jojaVanilla.data));
     }
 
     [Test]
     public void LoadVanillaOasis()
     {
         var oasisVanilla = new StardewData(true, Shop.OASIS);
-        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1));
+        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1), message=string.Format("Found crops: {0}", oasisVanilla.data));
     }
 
     [Test]
     public void LoadExpandedPierres()
     {
         var pierresExpanded = new StardewData(false, Shop.PIERRE);
-        Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1));
+        Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1), message=string.Format("Found crops: {0}", pierresExpanded.data));
     }
 
     [Test]
     public void LoadExpandedJoja()
     {
         var jojaExpanded = new StardewData(false, Shop.JOJAMART);
-        Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1));
+        Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1), message=string.Format("Found crops: {0}", jojaExpanded.data));
     }
 
     [Test]
     public void LoadExpandedOasis()
     {
         var oasisExpanded = new StardewData(false, Shop.OASIS);
-        Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1));
+        Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1), message=string.Format("Found crops: {0}", oasisExpanded.data));
     }
 
     [Test]
