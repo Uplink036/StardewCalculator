344f8e42fe529c5b6e825e9a50aa6afecf6ebd70
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-09-16 13:26:38 +0200

    Uppdate load shop messages

--- StardewCalculator.Tests/StardewDataTests.cs
+++ StardewCalculator.Tests/StardewDataTests.cs
@@ -7,42 +7,42 @@ public class StardewDataTests
     public void LoadVanillaPierres()
     {
         var pierresVanilla = new StardewData(true, Shop.PIERRE);
-        Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6), string.Format("Found crops: {0}", pierresVanilla.data));
+        Assert.That(pierresVanilla.data.Count(), Is.EqualTo(6), $"Loaded: {pierresVanilla}"));
     }
 
     [Test]
     public void LoadVanillaJoja()
     {
         var jojaVanilla = new StardewData(true, Shop.JOJAMART);
-        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1), string.Format("Found crops: {0}", jojaVanilla.data));
+        Assert.That(jojaVanilla.data.Count(), Is.EqualTo(1), $"Loaded: {jojaVanilla}");
     }
 
     [Test]
     public void LoadVanillaOasis()
     {
         var oasisVanilla = new StardewData(true, Shop.OASIS);
-        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1), string.Format("Found crops: {0}", oasisVanilla.data));
+        Assert.That(oasisVanilla.data.Count(), Is.EqualTo(1), $"Loaded: {oasisVanilla}");
     }
 
     [Test]
     public void LoadExpandedPierres()
     {
         var pierresExpanded = new StardewData(false, Shop.PIERRE);
-        Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1), string.Format("Found crops: {0}", pierresExpanded.data));
+        Assert.That(pierresExpanded.data.Count(), Is.EqualTo(1), $"Loaded: {pierresExpanded}");
     }
 
     [Test]
     public void LoadExpandedJoja()
     {
         var jojaExpanded = new StardewData(false, Shop.JOJAMART);
-        Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1), string.Format("Found crops: {0}", jojaExpanded.data));
+        Assert.That(jojaExpanded.data.Count(), Is.EqualTo(1), $"Loaded: {jojaExpanded}");
     }
 
     [Test]
     public void LoadExpandedOasis()
     {
         var oasisExpanded = new StardewData(false, Shop.OASIS);
-        Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1), string.Format("Found crops: {0}", oasisExpanded.data));
+        Assert.That(oasisExpanded.data.Count(), Is.EqualTo(1), $"Loaded: {oasisExpanded}");
     }
 
     [Test]
